e018993d0c3cb91979bbbbbebbfac2437c85030d
iitjee <iit.jee.help.me@gmail.com>
2016-12-10 09:07:39 +0530

    Create 02NetworkingBasics.java

---
+++ 02NetworkingBasics.java
@@ -0,0 +1,11 @@
+/*
+1. JSON parsing
+2. HTML Networking 
+3. Threads and Parallelism
+4. Preferences
+*/
+
+/*Earthquake
+USGS Earthquake real time feeds and notifications: http://earthquake.usgs.gov/earthquakes/feed/v1.0/index.php
+USGS Real-Time Earthquake Data in Spreadsheet Format: http://earthquake.usgs.gov/earthquakes/feed/v1.0/csv.php
+*/
